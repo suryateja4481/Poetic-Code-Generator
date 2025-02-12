8c63bf6bcb3cb87546a18c38a1282ce17f9e79c1
suryateja4481 <steja4481@gmail.com>
2025-02-12 13:50:10 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Poetic-Code-Generator
+# co6d6447980497dd5775ab78d
+
+Quick start:
+
+```
+$ npm install
+$ npm start
+````
+
+
+
---
+++ index.js
@@ -0,0 +1,14 @@
+import { generateTextAndImage } from "./utils.js"
+
+
+let name = prompt("Enter Your Name")
+
+
+let favoriteActivity = prompt("Enter Your Favorite Activity")
+
+let favoritePlace = prompt("Enter Your Favarite Place")
+
+let temperature = 0.6
+
+
+generateTextAndImage(name, favoriteActivity, favoritePlace, temperature)
